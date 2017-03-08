17fe598bb56ad1d1c506d833388c27bdca261fda
rivero.adan <rivero.adan@hotmail.com>
2017-03-08 14:54:43 -0300

    aa

--- Seleniumtest/Seleniumtest/UnitTest1.cs
+++ Seleniumtest/Seleniumtest/UnitTest1.cs
@@ -76,8 +76,7 @@ namespace Seleniumtest
             driver.Navigate().Forward();
             driver.Navigate().Refresh();
             driver.Close();
-           //asdadsasdasd
-           //asdadasdasdasd
+           
         }
 
         [TestMethod]
